ad3ffaa9d3fb2905b03a9399c5173f9323612303
João Ulisses <sirnobin@gmail.com>
2024-06-18 17:44:27 -0300

    First commit
    simples test created

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,9 @@
+describe('My First Test Suite', function () {
+    it('My first test case', function() {
+        expect(true).to.equal(true)
+    })
+
+    it('My second test case', function() {
+        expect(true).to.equal(false)
+    })
+})
