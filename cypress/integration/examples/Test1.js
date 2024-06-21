35a53d9bdf653e8820f44e6705b2c6901e0a0ed4
João Ulisses <sirnobin@gmail.com>
2024-06-20 22:44:04 -0300

    type into search box and validating results

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,9 +1,16 @@
+/// <reference types="Cypress" />
+
 describe('My First Test Suite', function () {
     it('My first test case', function() {
         expect(true).to.equal(true)
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        cy.get('.product:visible').should('have.length', 4)
+        //fixture
     })
 
-    it('My second test case', function() {
-        expect(true).to.equal(false)
-    })
+    // it('My second test case', function() {
+        // expect(true).to.equal(false)
+    // })
 })
