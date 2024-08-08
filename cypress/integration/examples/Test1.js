a202f9a63e095bd1d96b13cc636f12eabbcf4808
João Ulisses <sirnobin@gmail.com>
2024-08-08 14:23:25 -0300

    add more asserts

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,5 @@
 /// <reference types="Cypress" />
-
+ 
 describe('My First Test Suite', function () {
     it('My first test case', function() {
         expect(true).to.equal(true)
@@ -7,13 +7,27 @@ describe('My First Test Suite', function () {
         cy.get('.search-keyword').type('ca')
         cy.wait(2000)
         cy.get('.product:visible').should('have.length', 4)
-        // Below code it's a better strategy than the code above
-        cy.get('.products').find('.product').should('have.length', 4)
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
-        //fixture
+        
+        cy.get('.products').as('productLocator')
+        cy.get('@productLocator').find('.product').should('have.length',4)
+        cy.get(':nth-child(3) > .product-action > button').click()
+        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click().then(function() {
+            console.log('sf')
+        })
+ 
+        cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+            const textVeg=$el.find('h4.product-name').text()
+            if (textVeg.includes('Cashews')) {
+                $el.find('button').click()
+            }
+        })
+ 
+        //assert if logo text is correctly displayed
+        cy.get('.brand').should('have.text','GREENKART')
+        
+        //this is to print in logs
+        cy.get('.brand').then(function(logoelement){
+            cy.log(logoelement.text())
+        })
     })
-
-    // it('My second test case', function() {
-        // expect(true).to.equal(false)
-    // })
 })
