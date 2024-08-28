71ff49627698ca0dfa50b1d086254941316891b5
João Ulisses <sirnobin@gmail.com>
2024-08-27 21:11:20 -0300

    Web Table

---
+++ cypress/integration/examples/WebTable.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress" />
+ 
+describe('My First Test Suite', function () {
+    it('My first test case', function() {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+        cy.get('tr td:nth-child(2)').each(($el, index, $list) => {
+            const text = $el.text()
+            if (text.includes("Python")) {
+                cy.get("tr td:nth-child(2").eq(index).next().then(function(price){
+                    const priceText = price.text()
+                    expect(priceText).to.equal('25')
+                })
+            }
+        })
+    })
+})
