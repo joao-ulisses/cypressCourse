2d1ea46143b01c2de07987dd150aaad0156370f9
João Ulisses <sirnobin@gmail.com>
2024-09-21 20:23:08 -0300

    add calendar based test

---
+++ cypress/integration/examples/Calendar.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+ 
+describe('Calendar Test Suite', function () {
+    it('Verify Calendar date', function() {
+        const monthNumber = "6";
+        const date = "15";
+        const year = "2027";
+        const expectedList = [monthNumber, date, year];
+        
+        cy.visit(Cypress.env('url') +  '#/offers')
+        cy.get(".react-date-picker__inputGroup").click();
+        cy.get(".react-calendar__navigation__label").click().click();
+        cy.contains("button", year).click()
+        cy.get(".react-calendar__year-view__months__month").eq(Number(monthNumber)-1).click();
+        cy.contains("abbr", "15").click();
+
+        // Assertion
+        cy.get(".react-date-picker__inputGroup__input").each(($el, index) => {
+            cy.wrap($el).invoke('val').should('eq', expectedList[index]);
+        })
+    })
+})
